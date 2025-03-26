73242e0115a9c1428030fcd5aaee1075525e76a1
justin brewer <justinbrewer49@gmail.com>
2025-03-26 19:55:00 -0400

    story

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,27 @@
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        public static void madlib()
+        { 
+            Console.WriteLine("what is your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("whats your favorite food");
+            string food = Console.ReadLine();
+            Console.WriteLine("what are your hobbies");
+            string hobbies = Console.ReadLine();
+            Console.WriteLine("do you have a pet");
+            string pet = Console.ReadLine();
+            Console.WriteLine("what day was it");
+            string day = Console.ReadLine();
+            Console.WriteLine("what time was it");
+            string time = Console.ReadLine();
+            Console.WriteLine("what was the weather like");
+            string weather = Console.ReadLine();
+            
+            
+            Console.WriteLine($" on {day} {name} was eating {food} he was eating after playing {hobbies} around {time} it was {weather} his {pet} jumped up and took his {food}. It was a great day");
         }
+        static void Main(string[] args){
+            madlib();}
     }
 }
