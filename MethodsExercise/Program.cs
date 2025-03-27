9182856534ed1df09a4ebd1b25aec8047f9a0426
justin brewer <justinbrewer49@gmail.com>
2025-03-26 20:16:40 -0400

    Completed Exercise 2 with math operations methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MethodsExercise
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace MethodsExercise
 {
     public class Program
     {
@@ -22,7 +24,37 @@
             
             Console.WriteLine($" on {day} {name} was eating {food} he was eating after playing {hobbies} around {time} it was {weather} his {pet} jumped up and took his {food}. It was a great day");
         }
+        public static int divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Add(int a, int b)
+        { 
+            return a + b;
+        }
         static void Main(string[] args){
-            madlib();}
+            //madlib();
+            int a = 5;
+            int b = 5;
+            int addint = Add(5, 5);
+            int subtractint = subtract(5, 5);
+            int multiplyint = multiply(5, 5);
+            int divideint = divide(5, 5);
+            Console.WriteLine(addint);
+            Console.WriteLine(subtractint);
+            Console.WriteLine(multiplyint);
+            Console.WriteLine(divideint);
+        }
     }
 }
